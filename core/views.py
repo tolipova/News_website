ba1bb820611cc9547be409c37ac1f26101efb661
Tolipova Shahina <143376267+tolipova@users.noreply.github.com>
2023-10-30 13:53:54 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,7 +3,13 @@ from .models import *
 # Create your views here.
 def info(request):
     posts = Posts.objects.all()
+    feature = Featured.objects.all()
+    silder = MainSilder.objects.all()
+    starts = StartSlider.objects.all()
     context = {
-        'posts':posts
+        'posts':posts,
+        'feature':feature,
+        'silder':silder,
+        'starts':starts
     }
     return render (request, 'index.html', context)
