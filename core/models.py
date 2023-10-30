2be386b8ffbafe3ac5420369dfea0b03e2a1f8b4
Tolipova Shahina <143376267+tolipova@users.noreply.github.com>
2023-10-30 13:54:19 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -6,11 +6,36 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+    
+class MainSilder(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='user_info/')
+    date = models.DateTimeField(auto_now=False)
+    title = models.CharField(max_length=255)
+    info = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+class StartSlider(models.Model):
+    image = models.ImageField(upload_to='user_info/')
+    info = models.CharField(max_length=255)
 
 class Posts(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     date = models.DateField(auto_now=False)
+    image = models.ImageField(upload_to='user_info/')
 
     def __str__(self):
         return self.title
+
+class Featured(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='user_info/')
+    title = models.CharField(max_length=255)
+    date = models.DateField(auto_now=False)
+    info = models.TextField()
+
+    def __str__(self):
+        return self.name
