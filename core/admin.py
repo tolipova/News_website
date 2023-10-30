f369865405b84d57d981653351b37e6781a75ba5
Tolipova Shahina <143376267+tolipova@users.noreply.github.com>
2023-10-30 13:54:42 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,19 @@ from django.utils.html import mark_safe
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('category')
 
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('category','title','date','get_html_photo',)
+
+    def get_html_photo(self,object):
+        if object.image:
+            return mark_safe(f'<img src="{object.image.url}" width="50">')
+        return None
+    get_html_photo.short_description = 'Surat'
+
 admin.site.register(Posts)
 admin.site.register(Category)
+admin.site.register(Featured)
+admin.site.register(MainSilder)
+admin.site.register(StartSlider)
+
 
